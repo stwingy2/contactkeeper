ad3d4e31bac0c87f9367518bb1d8c80108264edd
stwingy <john@scallywag.org.uk>
2020-01-27 20:46:21 +0000

    connected mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
+
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
